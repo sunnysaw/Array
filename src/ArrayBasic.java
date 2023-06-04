10ca48b075b3912e27c675256be80a3cf2de6a90
sunny gupta <sunnyroy8084401051@gmail.com>
2023-06-04 14:25:26 +0530

    Initial commit

---
+++ src/ArrayBasic.java
@@ -0,0 +1,14 @@
+/*
+Question : Given an array of N integers where each value represents the number of chocolates in a packet. Each packet
+           have a variable number of chocolates. There are m students, the task is to distribute chocolate packets
+           such that
+           1 : Each student gets one packet.
+           2 : The difference between the number of chocolates in the packet with maximum chocolates and the packet
+               with minimum chocolates given to the students is minimum.
+________________________________________________________________________________________________________________________
+I/P = {7,3,2,4,9,12,56}, Student = 3
+O/P = Minimum difference is 2
+ */
+
+public class ArrayBasic {
+}
